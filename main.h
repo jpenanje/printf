80ac670cd7cd3967a2eff4173c64247d1cd9b016
jpenanje <jpenanje@gmail.com>
2022-04-22 23:50:47 +0100

    update task 0

--- main.h
+++ main.h
@@ -5,5 +5,8 @@
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
-
+int print_buf(char *buf, unsigned int nbuf);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
+int ev_print_func(const char *s, int index);
 #endif
